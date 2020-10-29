1215ba8c91b089698c4ba84ec9f235dd86bb93dc
Patrick Swaney <prswaney@gmail.com>
2020-10-29 13:34:56 -0700

    change the replacement symbol from a hyphen to an underscore.

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
       let alternaword = [];
       for (let index2 = 0; index2 < word.length; index2++) {
         if (word[index2] === "A" || word[index2] === "E" || word[index2] === "I" || word[index2] === "O" || word[index2] === "U" || word[index2] === "a" || word[index2] === "e" || word[index2] === "i" || word[index2] === "o" || word[index2] === "u") {
-          alternaword.push("-");
+          alternaword.push("_");
         } else {
           alternaword.push(word[index2]);
           }
